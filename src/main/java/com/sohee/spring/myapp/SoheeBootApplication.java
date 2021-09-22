8b1e3e9c068fd86465dea826d91b600719d6b817
kk1112k <kk1112k@naver.com>
2021-09-22 19:06:19 +0900

    스프링부트 처음

---
+++ src/main/java/com/sohee/spring/myapp/SoheeBootApplication.java
@@ -0,0 +1,33 @@
+package com.sohee.spring.myapp;
+ 
+import javax.sql.DataSource;
+ 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+ 
+@SpringBootApplication
+public class SoheeBootApplication {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(SoheeBootApplication.class, args);
+        
+    }
+    
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        
+        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/*.xml");
+        sessionFactory.setMapperLocations(res);
+        
+        return sessionFactory.getObject();
+    }
+ 
+}
+ 
---
+++ src/test/java/com/sohee/spring/myapp/SoheeBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sohee.spring.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoheeBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
